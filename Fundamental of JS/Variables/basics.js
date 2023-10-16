174c513101791cdefd961246d8b8ab4a2b03f323
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-10-16 10:39:41 +0530

    let, const and var variables

--- Fundamental of JS/Variables/basics.js
+++ Fundamental of JS/Variables/basics.js
@@ -1,9 +1,65 @@
-console.log("JS Fundamentals!!");
+// console.log("JS Fundamentals!!");
 
-let age = 23;
-console.log(age);
+// let age = 23;
+// console.log(age);
 
-age = "twenty three";
-console.log(age);
+// age = "twenty three";
+// console.log(age);
 
  
+// let, const and var variable
+let age;  // declaration
+
+age = 10;   // definition
+console.log(age);
+
+
+// let vs const
+// const pi = 3.14;
+
+// /*  TypeError: Assignment to constant variable. */
+// pi = 32;
+
+/* SyntaxError: Missing initializer in const declaration */
+// const year;
+// year = 1999;
+
+
+/* SyntaxError: Identifier 'a' has already been declared  */
+// let a = 10;
+// let a = 40;
+
+/* SyntaxError: Identifier 'year' has already been declared */
+// const year = 200;
+// const year = 200;
+
+/* let vs const
+   we can declare and define/ initialize let variable later.
+
+   we have to declare and define/initialize const variable in same line.
+
+   let and const variables cannot be redeclare.
+
+
+   we can redeclare var variables, declare and define/initialize it later
+
+*/
+
+// var a = 10;
+// var a = 12;
+
+// console.log(12);
+  
+
+// var a;
+// a = 20;
+
+// console.log(a);
+
+
+// var myName = "Kartik";
+
+// myName = "Krishna";
+
+// console.log(myName);
+

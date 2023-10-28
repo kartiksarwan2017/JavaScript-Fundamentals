9faf3eaed31db7c75f5699d33f703bfe972e9aef
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-10-28 13:32:54 +0530

    Data Types

---
+++ Fundamental of JS/Data Types/index.js
@@ -0,0 +1,46 @@
+/* Primitive Datatypes */
+
+// Number (64 bit)
+
+// let num = 10;
+// console.log(typeof num);
+
+// String
+// const firstName = "Hare";
+// const lastName = 'Krishna';
+// const nickName = `Krishna`;
+
+
+// console.log(typeof firstName);
+// console.log(typeof lastName);
+// console.log(typeof nickName);
+
+
+// const str = "krishna's book";
+// console.log(str);
+
+
+// Boolean
+// let bool = false;
+// console.log(typeof bool);
+
+// undefined  value and type of variable a is undefined
+let a;
+console.log(typeof a);
+console.log(a);
+
+// Null
+let b = null;
+console.log(b);  // null
+console.log(typeof b);   // object
+
+
+// Symbol
+
+// BigInt (ES2020)
+// if the number ends with n then its type is considered as bigInt
+let num1 = 10n;
+console.log(typeof num1);
+
+
+
